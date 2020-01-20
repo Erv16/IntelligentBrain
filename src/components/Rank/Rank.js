eba4c56dc4068f86797443c446b1e27ebcaacdf0
Erv16 <epalani1@binghamton.edu>
2020-01-20 12:46:48 +0530

    Built the initial Particles, navigation, logo components and created the basic layout for the front end

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>{'Erwin, your current rank is...'}</div>
+      <div className='white f1'>{'#16'}</div>
+    </div>
+  );
+};
+
+export default Rank;
